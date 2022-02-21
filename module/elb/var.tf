99e4ec00fe0b4388785c5d82c4dad97c2910854e
sayone <viveksayone1@gmail.com>
2022-02-21 10:20:51 +0530

    iam user policy and elb creation

---
+++ module/elb/var.tf
@@ -0,0 +1,17 @@
+variable "lb_sg" {
+  description = "lb sg"
+}
+variable "lb_subnet" {
+  description = "lb subnet"
+}
+variable "lb_subnet2" {
+  description = "lb subnet"
+}
+
+variable "lb_vpc" {
+  description = "lb vpc"
+}
+
+variable "lb_instance" {
+  description = "new ln instance"
+}
---
+++ module/s3/var.tf
@@ -0,0 +1,4 @@
+variable "bucket" {
+  description = "aws s3 bucket-name"
+}
+ 
